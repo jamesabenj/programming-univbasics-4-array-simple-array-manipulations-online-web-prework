11dc36f48dd32c93a314f75d0d25a685ca04970a
James Benjamin <james.abenj@gmail.com>
2019-09-06 12:16:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,9 +28,8 @@ def using_concat(array1, array2)
  array1.concat(array2)
   end
   
-  def using_insert
-    array = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-    array.insert(4, "Python")
+  def using_insert(array, element)
+    array.insert(4, element)
   end
   
   def using_uniq
